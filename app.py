c33f7964ccf6686e374bd6806adc00e4a30c2b8b
sameerb3737 <36567068+sameerb3737@users.noreply.github.com>
2018-02-21 14:40:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def makeWebhookResult(req):
         "messages": [
         {
           "type": "0",
-          "platform": "facebook",
+          
           "speech": "my first response"
         }        
         ],
