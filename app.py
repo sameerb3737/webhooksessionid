7635aa2babe4f8f5df1d6b64756c2137bfb9d447
sameerb3737 <36567068+sameerb3737@users.noreply.github.com>
2018-02-21 11:23:01 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -28,13 +28,16 @@ def webhook():
     return r
 
 def makeWebhookResult(req):
-    if req.get("result").get("action") != "delivery.options":
+    if req.get("result").get("action") != "shipping.cost":
         return {}
     result = req.get("result")
-   
+    parameters = result.get("parameters")
+    zone = parameters.get("shipping-zone")
 
-    
-     speech = "ok"
+    cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
+
+    speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
+
     print("Response:")
     print(speech)
 
